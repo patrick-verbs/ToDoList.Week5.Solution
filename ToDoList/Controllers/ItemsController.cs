dde78bf27c22fc2e5910a017760e2b0fd48c8bd1
Patrick <34055545+patrick-verbs@users.noreply.github.com>
2021-06-01 09:31:11 -0700

    Initial commit

---
+++ ToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,125 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace ToDoList.Controllers
+{
+  public class ItemsController : Controller
+  {
+    private readonly ToDoListContext _db;
+
+    public ItemsController(ToDoListContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Items.OrderBy(item => item.DateDue).ToList());
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Item item, int CategoryId)
+    {
+      _db.Items.Add(item);
+      _db.SaveChanges();
+      if (CategoryId != 0)
+      {
+        _db.CategoryItem.Add(new CategoryItem() { CategoryId = CategoryId, ItemId = item.ItemId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisItem = _db.Items
+        .Include(item => item.JoinEntities)
+        .ThenInclude(join => join.Category)
+        .FirstOrDefault(item => item.ItemId == id);
+      return View(thisItem);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      return View(thisItem);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Item item, int CategoryId)
+    {
+      CategoryItem joinTableEntry = null;
+      try
+      {
+        joinTableEntry = _db.CategoryItem.Where(entry => (entry.ItemId == item.ItemId && entry.CategoryId == CategoryId)).Single();
+      }
+      catch (System.Exception)
+      {
+        Console.WriteLine("Doesn't exist in CategoryItem entry in table");
+      }
+
+      if (CategoryId != 0 && joinTableEntry == null)
+      {
+        _db.CategoryItem.Add(new CategoryItem() { CategoryId = CategoryId, ItemId = item.ItemId });
+      }
+      _db.Entry(item).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddCategory(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      return View(thisItem);
+    }
+
+    [HttpPost]
+    public ActionResult AddCategory(Item item, int CategoryId)
+    {
+      if (CategoryId != 0)
+      {
+        _db.CategoryItem.Add(new CategoryItem() { CategoryId = CategoryId, ItemId = item.ItemId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      return View(thisItem);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      _db.Items.Remove(thisItem);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteCategory(int joinId)
+    {
+      var joinEntry = _db.CategoryItem.FirstOrDefault(entry => entry.CategoryItemId == joinId);
+      _db.CategoryItem.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
+
---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+  public class Item
+  {
+    public Item()
+    {
+      this.JoinEntities = new HashSet<CategoryItem>();
+    }
+
+    public int ItemId { get; set; }
+    public string Description { get; set; }
+
+    [Display(Name = "Task Completed")]
+    public bool IsCompleted { get; set; } = false;
+    public DateTime DateDue { get; set; } = DateTime.Now;
+
+    public virtual ICollection<CategoryItem> JoinEntities { get; }
+  }
+}
